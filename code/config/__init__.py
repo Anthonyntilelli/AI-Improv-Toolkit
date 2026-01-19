afdc86f44077770bd69535cd5a980b2b52da079b
Anthony Tilelli <Anthony@tilelli.me>
2026-01-19 22:04:03 +0000

    Refactor ingest configuration: update key options to use evdev key codes and enhance button settings

--- code/config/__init__.py
+++ code/config/__init__.py
@@ -4,6 +4,7 @@ from .config import (
     ButtonResetSubSettings,
     KeyOptions,
     ButtonAvatarSubSettings,
+    AllowedActions,
 )
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "ButtonResetSubSettings",
     "KeyOptions",
     "ButtonAvatarSubSettings",
+    "AllowedActions",
 ]
--- code/ingest/ingest.py
+++ code/ingest/ingest.py
@@ -5,33 +5,85 @@ Use the main function to start the ingest process.
 
 # evdev API reference: https://python-evdev.readthedocs.io/en/latest/usage.html
 
+import asyncio
+from contextlib import contextmanager
 from dataclasses import dataclass
-from time import sleep
+from typing import Generator
 
 import evdev
 
 from config import KeyOptions as KO
-# from config import ButtonResetSubSettings as RBC
-# from config import ButtonAvatarSubSettings as BAC
+
 from config import config as cfg
-# from ._config import load_internal_config, _AudioSettings as AudioSettings
+from ._config import load_internal_config, _AudioSettings as AudioSettings
+
 
 @dataclass
-class Button:
+class _Button:
     device: evdev.InputDevice
-    path: str
-    key: dict[KO, str] # key with action name
-    grabbed: list[bool]
+    key: dict[KO, str]  # key with action name
+    grabbed: bool
     last_pressed: int = 0
 
+
+@contextmanager
+def button_devices(config: cfg.Config) -> Generator[list[_Button], None, None]:
+    """Context manager to handle button devices (grab device)."""
+    devices: list[_Button] = []
+    try:
+        # Reset button
+        devices.append(
+            _Button(
+                evdev.InputDevice(config.Buttons["Reset"]["Path"]),
+                {config.Buttons["Reset"]["Key"]: "reset"},
+                config.Buttons["Reset"]["grab"],
+            )
+        )
+
+        # Avatar buttons
+        for avatar_button in config.Buttons["Avatars"]:
+            devices.append(
+                _Button(
+                    evdev.InputDevice(avatar_button["Path"]),
+                    {avatar_button["Speak"]: "speak"},
+                    avatar_button["grab"],
+                )
+            )
+
+        for device in devices:
+            if device.grabbed:
+                device.device.grab()
+        yield devices
+
+    finally:
+        for button in devices:
+            if button.grabbed:
+                button.device.ungrab()
+
+async def print_events(device: evdev.InputDevice) -> None:
+    async for event in device.async_read_loop():
+        print(device.path, evdev.categorize(event), sep=': ')
 
 def main(config: cfg.Config) -> None:
     """Main function to start the ingest role."""
     print("Ingest role started.")
 
-    # reset_config: AudioSettings = load_internal_config(config.Show["Actors_count"])
-    # avatar_count = len(config.Buttons["Avatars"])
+    loop = asyncio.get_event_loop()
 
-    sleep(5)
+    # TODO: set signal handlers for graceful shutdown
+
+
+    AudioConfig: AudioSettings = load_internal_config(config.Show["Actors_count"])
+    print(f"Loaded audio config: {AudioConfig}")
+
+    with button_devices(config) as buttons:
+        for device in buttons:
+          asyncio.ensure_future(print_events(device.device))
+
+    try:
+      loop.run_forever()
+    finally:
+      loop.run_until_complete(loop.shutdown_asyncgens())
+      loop.close()
 
     print("Ingest role completed.")
